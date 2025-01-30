8e8665a6dbec8b1e6df21b2b56aca542cca5635f
Jane Ahone <jane.eloundou@gmail.com>
2025-01-30 10:59:39 +0100

    Improve README

--- stage-0/index.js
+++ stage-0/index.js
@@ -1,17 +1,28 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors()); // Enable CORS
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({
-    email: "jane.eloundou@example.com",
-    current_datetime: new Date().toISOString(),
-    github_url: "https://github.com/yourusername/your-repo",
+  try {
+    res.status(200).json({
+      email: "jane.eloundou@gmail.com",
+      current_datetime: new Date().toISOString(),
+      github_url: "https://github.com/jane-ahone/HNG-12-Stage-0",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something went wrong. Please try again later.",
+    });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Endpoint not found",
   });
 });
 
